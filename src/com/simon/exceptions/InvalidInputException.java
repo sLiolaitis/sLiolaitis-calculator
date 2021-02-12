f93c24d58e6e91ece147255d2c8aeaf9487e9aa4
Simonas Liolaitis <simonas.liolaitis@gmail.com>
2021-02-12 16:35:03 +0200

    deleting
    deleting

--- src/com/simon/exceptions/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package com.simon.exceptions;
-
-public class InvalidInputException extends Exception{
-
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
--- src/com/simon/input/Input.java
+++
@@ -1,7 +0,0 @@
-package com.simon.input;
-
-public interface Input {
-
-    String input () ;
-
-}
